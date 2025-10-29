a2daa0fdd5347832ecde89650ce4d6473d57cc79
Pulakkantipramanik <pulakkantipramanik@gmail.com>
2025-10-29 10:21:19 +0530

    Initial commit: Spring Boot + MySQL + Docker setup

---
+++ src/main/java/com/example/microservicebasic/MicroserviceBasicApplication.java
@@ -0,0 +1,11 @@
+package com.example.microservicebasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceBasicApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceBasicApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/microservicebasic/HelloControllerTest.java
@@ -0,0 +1,13 @@
+package com.example.microservicebasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
